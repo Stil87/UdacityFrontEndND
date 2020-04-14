4bc641526c660d1188679c3b3f04e5f7f2074e29
tillschmidt <schmidt.till.mail@gmail.com>
2020-04-14 13:39:24 +0200

    webpack prod running with main.css optimizaiton

---
+++ Project4/webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizerCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+console.log(__dirname)
+
+module.exports = {
+   
+    entry: './client/index.js',
+    context: path.resolve(__dirname, './src'),
+    mode: 'production',
+    optimization: {minimizer:[
+        new TerserPlugin({}), new OptimizerCSSAssetsPlugin({})]
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    devServer: {
+        contentBase: "./src/client/views",
+        hot: true
+    },
+    
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: "./client/views/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
+    ],
+
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+              },
+        ]
+    }
+}
